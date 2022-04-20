2dbe025d1ff6817c8cf044288ac5a991c603ff29
monenavarro <me.navarro.97@outlook.com>
2022-04-20 12:23:03 +0100

    Authentication feature implemented

--- app.rb
+++ app.rb
@@ -33,5 +33,20 @@ class MakersBnB < Sinatra::Base
     erb(:spaces) #Welcome... take the user information fro the user in the session.
   end
 
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    user = User.authenticate(email: params[:email], password: params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect '/spaces'
+    else
+      flash[:notice] = 'Please check your email or password.'
+      redirect '/sessions/new'
+    end
+  end
+
   run! if app_file == $0
 end
