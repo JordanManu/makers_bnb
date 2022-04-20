2b424ea2d4bc8046e18b43cdcad3c50279359db7
Jordan <jordanmanu@live.com.au>
2022-04-20 16:57:27 +0100

    Added feature test for availability dates

---
+++ spec/features/available_dates_spec.rb
@@ -0,0 +1,11 @@
+feature 'available dates' do
+  scenario 'it lists the dates the space is available' do
+    space = Space.create(name: 'Strawberry Fields Cottage', price: '150', description: 'Beautiful')
+    visit '/spaces/availability/new'
+    select "Strawberry Fields Cottage", :from => "My listings"
+    fill_in "Available date", :with => "25/4/2022"
+    click_button 'Submit'
+
+    expect(page).to have_content "Availability for Strawberry Fields Cottage has been added for 25/4/2022"
+  end
+end
